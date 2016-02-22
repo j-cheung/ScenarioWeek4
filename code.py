ebc6ee6c27571ee14721de45d2598776a44f4ae8
stupidjc <stupidjc@Johnsons-MacBook-Pro.local>
2016-02-22 12:35:19 +0000

    polygon vertices into tuples

--- code.py
+++ code.py
@@ -19,7 +19,27 @@ def readfile():
 			line = line.lstrip(': ')
 
 			polygons.append(line)
-		for j in polygons:
-			print j + '\n' 
+		
+		s = polygons[0]
+		print s
+		vertices = []
+		tempStr = ''
+		inTuple = False
+		j = 0
+		while j < len(s):
+			if s[j] == '(':
+				inTuple = True
+				tempStr += s[j]
+			elif s[j] == ')':
+				tempStr += s[j]
+				vertices.append(tempStr)
+				inTuple = False
+				tempStr = ''
+			elif inTuple:
+				tempStr += s[j]
+			j += 1
+		print vertices
+		#print s
+		#print polygons[1] 
 
 readfile()
