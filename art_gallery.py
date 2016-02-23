37e463a36d15a16b54a7cae94519147206a5ac75
Gao Jun Tang <tgjun0327@gmail.com>
2016-02-23 11:31:23 +0000

    update part1

--- art_gallery.py
+++ art_gallery.py
@@ -3,8 +3,103 @@ from point import Point
 from triangulation import Triangulation
 from coloring import Coloring
 import threading
+import os, time, math
+from os.path import join, basename, exists, isdir
+
+INPUT_FILE_DIR = "input/"
+OUTPUT_FILE_NAME = "output/"
 
 class ArtGallery(object):
 
 	def __init__(self, point):
-		self._point
+		self._point = Point(point.id, point.x, point.y)
+		self._points = [point]
+		self._polygon = [(point.x, point.y)]
+		self._triangulation = Triangulation(self._points)
+		self._color = Coloring(self._points, self._triangulation)
+
+	def readguardsfile():
+		inputFileName = "guards"
+		infilename = os.path.join(INPUT_FILE_DIR, inputFileName + '.pol')
+		outfilename = os.path.join(INPUT_FILE_DIR, inputFileName + '.sol')
+		with open(infilename, 'r') as f:
+			polygons = []
+			for i in range(0,30):
+				line = f.readline()
+				line = line.rstrip()
+				while not line.startswith(':'):
+					line = line.lstrip("0123456789");
+				line = line.lstrip(': ')
+				polygons.append(line)
+			polygonVertices = []
+			for polygon in polygons:
+				s = polygon
+				vertices = []
+				tempStr = ''
+				inTuple = False
+				j = 0
+				while j < len(s):
+					if s[j] == '(':
+						inTuple = True
+						tempStr += s[j]
+					elif s[j] == ')':
+						tempStr += s[j]
+						vertices.append(tempStr)
+						inTuple = False
+						tempStr = ''
+					elif inTuple:
+						tempStr += s[j]
+					j += 1
+				polygonVertices.append(vertices)
+
+			return polygonVertices
+
+	def _update(self):
+		self._points.sort()
+		self._polygon = []
+		for p in self._points:
+			self._polygon.append((p.x, p.y))
+
+	def get_polygon(self):
+		with self.lock:
+			return self._polygon
+
+	def get_process_point(self):
+		with self.lock:
+			return self._point
+
+	def get_points(self):
+		with self.lock:
+			return self._points
+
+	def get_diagonals(self):
+		with self.lock:
+			return self._triangulation.get_diagonals()
+
+	def get_min_color(self):
+		with self.lock:
+			return self._color.get_min_color()
+
+	def is_guard(self, point):
+		with self.lock:
+			return point.color == self._color.get_min_color()	
+            
+if __name__ == '__main__':
+			        
+	print ("Starting...")
+	tmp = ArtGallery.readguardsfile()
+	g = ArtGallery(tmp.pop(0))
+	for p in tmp:
+		g.include(p)
+		        
+	for p in g.get_points():
+		if g.is_guard(p):
+		    print (p, " GUARD!")
+		else:
+		    print (p)
+		                
+	print ("Min_color = " + str(g._color.get_min_color()))
+	print ("color[0] = " + str(g._color.get_color_count(0)))
+	print ("color[1] = " + str(g._color.get_color_count(1)))
+	print ("color[2] = " + str(g._color.get_color_count(2)))
+	print ("color[3] = " + str(g._color.get_color_count(3)))
--- triangulation.py
+++ triangulation.py
@@ -5,172 +5,172 @@ from triangle import Triangle
 
 class Triangulation(object):
 
-    EPSILON = 0.0000000001
+        EPSILON = 0.0000000001
 
-	def __init__(self, points):
-		self.polygon = points
-		self.clean()
+        def __init__(self, points):
+            self.polygon = points
+            self.clean()
 
-	def clean(self):
-		self.diagonals = {}
-		self.sides = {}
-		seld.start = None
+        def clean(self):
+            self.diagonals = {}
+            self.sides = {}
+            seld.start = None
 
-	def addDiagonal(self, triangle):
-		#identity
-		diagonal = Side(triangle.point0, triangle.point2)
-		#map the triangulation 
-		self.diagonals[diagonal] = triangle
-		self.addTriangle(triangle)
+        def addDiagonal(self, triangle):
+            #identity
+            diagonal = Side(triangle.point0, triangle.point2)
+            #map the triangulation 
+            self.diagonals[diagonal] = triangle
+            self.addTriangle(triangle)
 
-	def addTriangle(self, triangle):
-		if self.start is None: self.start = triangle
-        # each side of each triangle can be related to:
-        # only one triangle (this side is part of original polygon) 
-        # or two triangles (this side is a diagonal, created by triangulation)
-        # map each side and correlated triangles
-		for side in triangle.sides():
-			tt = self.sides.get(side)
-			# side already mapped
-			if tt:
-				# add this new correlated triangle (this side is a diagonal)
-				self.sides[side] = (tt[0], triangle)
-			else:
-				# this side has just appeared. it can be a diagonal, 
-                # but its opposite has not been triangulated yet
-                self.sides[side] = (triangle, None)
+        def addTriangle(self, triangle):
+            if self.start is None: self.start = triangle
+            # each side of each triangle can be related to:
+            # only one triangle (this side is part of original polygon) 
+            # or two triangles (this side is a diagonal, created by triangulation)
+            # map each side and correlated triangles
+            for side in triangle.sides():
+                tt = self.sides.get(side)
+                # side already mapped
+                if tt:
+                    # add this new correlated triangle (this side is a diagonal)
+                    self.sides[side] = (tt[0], triangle)
+                else:
+                    # this side has just appeared. it can be a diagonal, 
+                    # but its opposite has not been triangulated yet
+                    self.sides[side] = (triangle, None)
 
-    def get_start(self):
-    	#return the first triangle cretated in the last triangulation
-        #return self._triangles[0]
-    	return self.start
+        def get_start(self):
+            #return the first triangle cretated in the last triangulation
+            #return self._triangles[0]
+            return self.start
 
-    def get_diagonals(self):
-    	return self.diagonals.keys()
+        def get_diagonals(self):
+            return self.diagonals.keys()
 
-    def get_neighbours(self, triangle):
-    	neighbours = []
-    	for side in triangle.sides():
-    		neighbour = self.get_opposite(triangle, side)
-    		if neighbour:
-    			neighbours.append(neighbour)
-    	return neighbours
+        def get_neighbours(self, triangle):
+            neighbours = []
+            for side in triangle.sides():
+                neighbour = self.get_opposite(triangle, side)
+                if neighbour:
+                    neighbours.append(neighbour)
+            return neighbours
 
-    def get_opposite(self, triangle, side):    
-    	tt = self.sides.get(side)
-    	if tt: 
-    		if tt[0] != triangle:
-    			return tt[0]
-    		elif tt[1]:
-    			return tt[1]
-    	return None
+        def get_opposite(self, triangle, side):    
+            tt = self.sides.get(side)
+            if tt: 
+                if tt[0] != triangle:
+                    return tt[0]
+                elif tt[1]:
+                    return tt[1]
+            return None
 
 
-    def _set_indexes(self, v, n_points):
-        """ Set indexes for triangulation based on v parameter """
+        def _set_indexes(self, v, n_points):
+            """ Set indexes for triangulation based on v parameter """
 
-        u = v
-        if u >= n_points:
-            u = 0
+            u = v
+            if u >= n_points:
+                u = 0
 
-        v = u + 1
-        if v >= n_points:
-            v = 0
+            v = u + 1
+            if v >= n_points:
+                v = 0
 
-        w = v + 1
-        if w >= n_points:
-            w = 0
+            w = v + 1
+            if w >= n_points:
+                w = 0
 
-        return u, v, w
+            return u, v, w
 
-    def run(self):
-    	# clear the current triangulation
-        self.clean()
-        n_points = len(self.polygon)
-        # Minimum number of polygon to compose a triangle
-        if n_points < 3:
-            return False
+        def run(self):
+            # clear the current triangulation
+            self.clean()
+            n_points = len(self.polygon)
+            # Minimum number of polygon to compose a triangle
+            if n_points < 3:
+                return False
 
-        tmp_points = self.polygon[:]
-        # reverse if not clockwise orientation
-        if self.area() < 0.0:
-        	tmp_points.reverse()
+            tmp_points = self.polygon[:]
+            # reverse if not clockwise orientation
+            if self.area() < 0.0:
+                tmp_points.reverse()
 
-        # controls the current triangle points inside the while statement
-        v = n_points - 1
-        attempts = (n_points + 3)
-        while n_points > 3:  
-        	if not attempts:
-        		self.clean()
-        		return False
-        	attempts -= 1 
+            # controls the current triangle points inside the while statement
+            v = n_points - 1
+            attempts = (n_points + 3)
+            while n_points > 3:  
+                if not attempts:
+                    self.clean()
+                    return False
+                attempts -= 1 
 
-            # start triangulation from the last triangulation point
-            # this is best in some cases (e.x. concave polygon)
-            # and worst in others (e.x. polygon with many inverses corners)...
-            #last_mark = v
-            u, v, w = self._set_indexes(v, n_points)
+                # start triangulation from the last triangulation point
+                # this is best in some cases (e.x. concave polygon)
+                # and worst in others (e.x. polygon with many inverses corners)...
+                #last_mark = v
+                u, v, w = self._set_indexes(v, n_points)
 
-            # Creates a Triangle object with three consecutives Point objects
-            triangle = Triangle(tmp_points[u], tmp_points[v], tmp_points[w])
+                # Creates a Triangle object with three consecutives Point objects
+                triangle = Triangle(tmp_points[u], tmp_points[v], tmp_points[w])
 
-            # check if this triangle can be cutted (snipped)
-            if self._snip(tmp_points, triangle):
-                # add current triangle to triangulation
-                self.addDiagonal(triangle) 
-                # remove triangle from polygon
-                tmp_points.remove(tmp_points[v])  
-                #v = last_mark # see above
-                # adjust the loop controls
-                n_points -= 1
-                attempts = (n_points + 3)
+                # check if this triangle can be cutted (snipped)
+                if self._snip(tmp_points, triangle):
+                    # add current triangle to triangulation
+                    self.addDiagonal(triangle) 
+                    # remove triangle from polygon
+                    tmp_points.remove(tmp_points[v])  
+                    #v = last_mark # see above
+                    # adjust the loop controls
+                    n_points -= 1
+                    attempts = (n_points + 3)
 
-        # add last triangle to triangulation
-        triangle = Triangle(tmp_points[0], tmp_points[1], tmp_points[2])
-        self.addTriangle(triangle)
-        del tmp_points
-        return True
+            # add last triangle to triangulation
+            triangle = Triangle(tmp_points[0], tmp_points[1], tmp_points[2])
+            self.addTriangle(triangle)
+            del tmp_points
+            return True
 
-    def _is_inside(self, point, triangle):
-    	ax, ay = triangle.point2.x - triangle.point1.x, triangle.point2.y - triangle.point1.y
-    	bx, by = triangle.point0.x - triangle.point2.x, triangle.point0.y - triangle.point2.y
-    	cx, cy = triangle.point1.x - triangle.point0.x, triangle.point1.y - triangle.point0.y    	    	
+        def _is_inside(self, point, triangle):
+            ax, ay = triangle.point2.x - triangle.point1.x, triangle.point2.y - triangle.point1.y
+            bx, by = triangle.point0.x - triangle.point2.x, triangle.point0.y - triangle.point2.y
+            cx, cy = triangle.point1.x - triangle.point0.x, triangle.point1.y - triangle.point0.y               
 
-        apx, apy = point.x - triangle.point0.x, point.y - triangle.point0.y
-        bpx, bpy = point.x - triangle.point1.x, point.y - triangle.point1.y
-        cpx, cpy = point.x - triangle.point2.x, point.y - triangle.point2.y
+            apx, apy = point.x - triangle.point0.x, point.y - triangle.point0.y
+            bpx, bpy = point.x - triangle.point1.x, point.y - triangle.point1.y
+            cpx, cpy = point.x - triangle.point2.x, point.y - triangle.point2.y
 
-        a_cross = ax*bpy - ay*bpx
-        b_cross = bx*cpy - by*cpx
-        c_cross = cx*apy - cy*apx
+            a_cross = ax*bpy - ay*bpx
+            b_cross = bx*cpy - by*cpx
+            c_cross = cx*apy - cy*apx
 
-        return a_cross >= 0 and b_cross >= 0 and c_cross >= 0
+            return a_cross >= 0 and b_cross >= 0 and c_cross >= 0
 
-    def _snip(self, points, triangle):
-        oppos = (triangle.point1.x - triangle.point0.x) * (triangle.point2.y - triangle.point0.y) 
-        adjac = (triangle.point1.y - triangle.point0.y) * (triangle.point2.x - triangle.point0.x)
+        def _snip(self, points, triangle):
+            oppos = (triangle.point1.x - triangle.point0.x) * (triangle.point2.y - triangle.point0.y) 
+            adjac = (triangle.point1.y - triangle.point0.y) * (triangle.point2.x - triangle.point0.x)
 
-        # the triangle makes a inverse corner (open corner)?
-        if self.EPSILON > (oppos - adjac):
-        	# if so, then this triangle isn't a ear of polygon...
-        	return False
+            # the triangle makes a inverse corner (open corner)?
+            if self.EPSILON > (oppos - adjac):
+                # if so, then this triangle isn't a ear of polygon...
+                return False
 
-        if point in points:
-        	if point not in triangle and self._is_inside(point, triangle):
-        		return False
-        return True
+            if point in points:
+                if point not in triangle and self._is_inside(point, triangle):
+                    return False
+            return True
 
-    def area(self):
-        size = len(self.polygon)
-        area = 0.0
-        i0 = size -1
-        i1 = 0
-        while i1 < size:
-            area += self.polygon[i0].x * self.polygon[i1].y - \
-                    self.polygon[i0].y * self.polygon[i1].x
-            i0 = i1 
-            i1 += 1
-        return (area / 2)        
+        def area(self):
+            size = len(self.polygon)
+            area = 0.0
+            i0 = size -1
+            i1 = 0
+            while i1 < size:
+                area += self.polygon[i0].x * self.polygon[i1].y - \
+                        self.polygon[i0].y * self.polygon[i1].x
+                i0 = i1 
+                i1 += 1
+            return (area / 2)            
 
 
 
